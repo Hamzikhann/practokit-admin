8b22ed224ead0588cd3852a532fc69b706b7a9f9
hamza-qasim <hamzaqasim.c@gmail.com>
2024-12-04 19:21:56 +0500

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  production: true,
-  reqBaseUrl: 'http://96.126.100.239:3030/api/v1/',
-  imgBaseUrl: 'http://96.126.100.239:3030/',
-  socketUrl: 'http://96.126.100.239:3030/',
-  uploadFileSize: 5000000,
-  dropBoxToken:
-    '4dLVexvzQZMAAAAAAAAAAb6AR3E0ttNIGn2xwztblEROwtviNgK2k8S-ZXzc7gsx',
-  dropBoxFolder: '/Apps/Assessment Tool/',
-  fileSource: 'dropbox',
+  // production: true,
+  // reqBaseUrl: 'http://96.126.100.239:3030/api/v1/',
+  // imgBaseUrl: 'http://96.126.100.239:3030/',
+  // socketUrl: 'http://96.126.100.239:3030/',
+  // uploadFileSize: 5000000,
+  // dropBoxToken:
+  //   '4dLVexvzQZMAAAAAAAAAAb6AR3E0ttNIGn2xwztblEROwtviNgK2k8S-ZXzc7gsx',
+  // dropBoxFolder: '/Apps/Assessment Tool/',
+  // fileSource: 'dropbox',
 };
